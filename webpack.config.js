45e43cf317dbce69f9c490a9939b15edaefcd286
Prasses Khadka <khadkaprasses@gmail.com>
2023-03-01 15:37:19 +0545

    Changed type:'asset/resource in webpack config to bundle images and still works fine.while importing forgot to put .png which was probably why it didn't work earlier

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports={
             },
             {
                 test:/\.(png|svg|jpg|jpeg|gif)$/i,
-                type:'asset/inline',
+                type:'asset/resource',
                 // use:['url-loader']
             }
         ]
