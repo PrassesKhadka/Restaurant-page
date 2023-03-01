ab1b705b1cfcead8900841d4c6e51f8bd2c18a26
Prasses Khadka <khadkaprasses@gmail.com>
2023-03-01 15:00:41 +0545

    Removed use:[url-loader] to load images in webpack instead used type:'asset/inline' and now the images load perfectly

--- VDOM.js
+++ VDOM.js
@@ -41,10 +41,35 @@ export default class vdom
         //Only when there is no child ,we can add innerText
         this.children=[]
         this.innerText=string
+        return this;
     }
 
     //This is where all the magic happens-->writing in the HTML page
+    //so the object which calls this will have all the 4 necessary conditions:element,attributes,children,innerText
+    //Now using this information rendre it in your browser -> DOM manipulation 
     render()
+    {
+        //Setting element
+        const DOM=document.createElement(this.element)
+        //Setting attributes
+        for(const i of Object.keys(this.attributes))
+        {
+            DOM.setAttribute(i,this.attributes[i])
+        }
+        //Setting innerText/Children these two are if and or
+        if(this.innerText===undefined)
+        {
+            //Here i is an object of class vdom that we will be passing
+            for(const j of this.children)
+            {
+                DOM.appendChild(j.render())
+            }
+        }else
+        {
+            DOM.textContent=this.innerText
+        }
+        return DOM;
+    }
 
 
 
--- home.js
+++ home.js
@@ -1,4 +1,19 @@
+import vdom from "./VDOM";
+const content=document.querySelector('#content')
 export default function loadHome()
 {
+    const home=new vdom('div')
+    .addAttributes({class:'home'})
+    .addChild(new vdom('img').addAttributes({class:'random-image',id:'1'}))
+    .addChild(new vdom('div').addAttributes({class:'description'}).addChild(new vdom('p').addText('Roadhouse is committed to maintaining the high quality and standards that our customers have become accustomed to.')))
+
+
+
+
+
+
+
+   
     
+    content.appendChild(home.render())
 }
