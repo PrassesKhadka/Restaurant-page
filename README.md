8228fccab1ac66e2eed4cffe1c28e625e89d6574
Prasses Khadka <khadkaprasses@gmail.com>
2023-03-17 22:15:25 +0545

    Adding babel-loader in the webpack.config object

--- README.md
+++ README.md
@@ -8,7 +8,6 @@ Link to the site: <a href="https://prasseskhadka.github.io/Restaurant-page/dist/
 
 This project was completed for <a href="https://www.theodinproject.com/">The Odin Project</a> Javascript Restaurant Page Assignment. 
 
-
 Skills:
 HTML,CSS,JS,Webpack
 Git GitHub
@@ -16,7 +15,7 @@ Git GitHub
 Lessons Learned:
 
 CLI(Command Line Interface):
-Using different command line interface to navigate through files and set up npm and git and github(Basic Git Workflow)
+Using different command line interface commands to navigate through files and set up npm and git and github(Basic Git Workflow)
 
 JS/ npm/ webpack:
 The goal of this project was to create a restaurant page using primarily JS w/ a focus on learning JS modules(allow you to break up your code into separate files. This makes it easier to maintain a code-base. Modules are imported from external files with the import statement.And exported using export default.Modules also rely on type="module" in the script tag.),webpack, and npm. HTML content is created dynamically using a virtual DOM completely written in JS. Along w/ DOM manipulation, I also focused on delegating functions in an object-oriented way using modules. This is also the first time wrapping my head around npm, webpack, json,and webpack.config.js  .
@@ -26,5 +25,5 @@ This page is designed to be responsive.
 Used clamp,flex-box,grid and,Media queries for a responsive layout.
 
 
-
-
+Updated:
+Adding Babel to webpack configuration with the babel-loader.
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,46 @@
-const path=require('path')
+const path = require('path')
 const { resourceUsage } = require('process')
-module.exports={
-    entry:"./src/index.js",
-    mode:'development',
-    output:{
-        filename:'main.js',
-        path:path.resolve(__dirname,'dist'),
+module.exports = {
+    entry: "./src/index.js",
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
     },
-    module:{
-        rules:[
+    module: {
+        rules: [
             //To load CSS
             {
-                test:/\.css$/i,
-                use:['style-loader','css-loader'],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             },
             //To load images 
             {
-                test:/\.(png|svg|jpg|jpeg|gif)$/i,
-                type:'asset',
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset',
                 // use:['url-loader']
             },
             //To load Fonts
             {
-                test:/\.(woff|woff2|eot|tff|otf)$/i,
-                type:'asset',
+                test: /\.(woff|woff2|eot|tff|otf)$/i,
+                type: 'asset',
+            },
+            //Adding babel-loader to the list of modules
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ],
+                        plugins:['@babel/plugin-proposal-class-properties']
+                    }
+                }
             },
         ]
     },
-    devtool:'inline-source-map',
+    devtool: 'inline-source-map',
 
 }
